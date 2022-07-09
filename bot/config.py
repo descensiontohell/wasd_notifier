8d8004021b7fdbcc3afbd1683d503ace8c011bf6
descensiontohell <isuhanen@gmail.com>
2022-07-09 11:28:19 +0300

    rework config

--- bot/config.py
+++ bot/config.py
@@ -2,6 +2,7 @@ import os
 from dataclasses import dataclass
 
 import yaml
+from envyaml import EnvYAML
 
 
 @dataclass
@@ -13,17 +14,16 @@ class Config:
 
 
 def setup_config() -> Config:
-    with open(os.path.join(os.getcwd(), "docker-compose.yml")) as conf:
-        raw_config = yaml.safe_load(conf)["services"]["db"]["environment"]
+    raw_config = EnvYAML(os.path.join(os.getcwd(), "docker-compose.yml"))
 
     if os.environ.get("IS_IN_DOCKER", False):
         db_host = "db"
     else:
         db_host = "localhost:6060"
 
-    db_user = raw_config["POSTGRES_USER"]
-    db_password = raw_config["POSTGRES_PASSWORD"]
-    db_name = raw_config["POSTGRES_DB"]
+    db_user = raw_config["services.db.environment.POSTGRES_USER"]
+    db_password = raw_config["services.db.environment.POSTGRES_PASSWORD"]
+    db_name = raw_config["services.db.environment.POSTGRES_DB"]
 
     return Config(
         host=db_host,
